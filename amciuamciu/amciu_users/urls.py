fa1fc799ea69fb130f389256f1fd80abde21176a
metalic24 <65673563+metalic24@users.noreply.github.com>
2021-11-29 19:31:59 +0100

    Revert "Revert "start""
    This reverts commit 447cfb8dbcc4232857197ff7fb84e66147975b19.

--- amciuamciu/amciu_users/urls.py
+++
@@ -1,7 +0,0 @@
-
-from django.urls import path, include
-from . import views
-
-urlspattern = [
-    path("register/", views.registration, name="register"),
-]
--- amciuamciu/amciu_users/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-def registration(request):
-    context ={}
-
-    render(request, "register.html",context)
