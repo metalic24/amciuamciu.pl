86a7b3f85549c6d765512d2e639014182574e949
metalic24 <65673563+metalic24@users.noreply.github.com>
2021-12-07 22:58:22 +0100

    relacje menu z restauracjami działa

--- amciuamciu/amciuamciu/settings.py
+++ amciuamciu/amciuamciu/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     #own
 
     'restaurant',
+    'menu',
+    
 ]
 
 MIDDLEWARE = [
---
+++ amciuamciu/menu/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import CharField, DecimalField, IntegerField, TextField
+from django.db.models.fields.related import ForeignKey
+from django.contrib.auth.models import User
+
+from restaurant.models import restaurant as rest
+
+
+
+class menu(models.Model):
+    name = CharField(null=False,max_length=120)
+    restaurant = ForeignKey(rest, on_delete=CASCADE, null=False)
+    price = DecimalField(max_digits=20, decimal_places=2, null=False)
+    description = TextField(null=True)
+
+
+
