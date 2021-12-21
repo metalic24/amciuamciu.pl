b7c202be4918f507e6be7769c11dd293347dbebb
metalic24 <65673563+metalic24@users.noreply.github.com>
2021-12-21 21:59:59 +0100

    działa zostawić xD

--- amciuamciu/users_amciu/views.py
+++ amciuamciu/users_amciu/views.py
@@ -1,14 +1,22 @@
+from django.contrib import auth
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from django.shortcuts import redirect, render
+
+from zamowienia.models import Order, Ordered_Items
 from .forms import  CreateUserForm
+from restaurant.views import show_restaurants
 
 def register(request):
-    form = CreateUserForm(request.POST)
-    if form.is_valid():
-        email = form.cleaned_data.get("email")
-        form.save()
-        return redirect(viev_login)
+    if request.user.is_authenticated:
+        return redirect(show_restaurants)
+    else:
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get("email")
+            new_user =form.save()
+            login(request, new_user)
+            return redirect(show_restaurants)
 
     context={
         'form':form
@@ -32,7 +40,7 @@ def viev_login(request):
 
         if user is not None:
             login(request,user)
-            return redirect(hello_login)
+            return redirect(show_restaurants)
         else:
             messages.info(request,'Username or password is incorrect')
             return  render(request,"login.html",context)
@@ -43,7 +51,26 @@ def viev_login(request):
     return  render(request,"login.html",context)
 
 def hello_login(request):
+    orders =[]
+    user_orders = Order.objects.filter(customer = request.user.id)
+    for order in user_orders:
+        ordered_items = Ordered_Items.objects.filter(order = order)
+        
+      
+          
+            
+
+    
+    
+   
+
     return render(request,"hello.html",{} )
+
+def log_out(request):
+    auth.logout(request)
+    return redirect(show_restaurants)
+    
+    
 
 
 
