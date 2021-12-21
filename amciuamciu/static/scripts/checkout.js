0e883895bcff7a4753b92d2559b95170b0620cbe
metalic24 <65673563+metalic24@users.noreply.github.com>
2021-12-21 17:07:20 +0100

    save

--- amciuamciu/static/scripts/checkout.js
+++ amciuamciu/static/scripts/checkout.js
@@ -2,6 +2,7 @@
 function display_cart()
 {
     let cartItems = localStorage.getItem("productInCart");
+   
     cartItems = JSON.parse(cartItems);
     let product_disp = document.querySelector('.products');
 
@@ -13,21 +14,36 @@ function display_cart()
             product_disp.innerHTML += `<tr class="product_row">
             <td>${item.name}</td>
             <td>${item.price} PLN</td>
-            <td>${item.in_cart}</td>
+            <td><ion-icon name="arrow-back-circle-outline"></ion-icon>
+            ${item.in_cart}<ion-icon name="arrow-forward-circle-outline"></ion-icon></td>
             <td>${item.price * item.in_cart} PLN</td>
-            </td>
+            <td><ion-icon name="close-circle-outline"></ion-icon><td>
+            </tr>
             `
             
 
         })
+       
+       disp_total();
+
+
+
+
         
     }
     
-   
 
-   
-
+}
+function disp_total()
+{
+    let totalCost = localStorage.getItem("totalCost");
+    let cost_disp = document.getElementById('totalCost')
     
+
+    console.log(cost_disp);
+
+    cost_disp.innerHTML = `<div>W sumie: ${totalCost}</div>`;
+
 }
 
 
