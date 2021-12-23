7f8acbabbc24f67302f149f5a2b2680a162d8a92
metalic24 <65673563+metalic24@users.noreply.github.com>
2021-12-23 17:32:03 +0100

    usuwanie z koszyka działa :+1:

--- amciuamciu/static/scripts/checkout.js
+++ amciuamciu/static/scripts/checkout.js
@@ -5,22 +5,22 @@ function display_cart()
    
     cartItems = JSON.parse(cartItems);
     let product_disp = document.querySelector('.products');
-
+    let i=1;
     if(cartItems)
     {
         product_disp.innerHTML ='<tr><th>Nazwa</h><th>cena</th><th>ilosc</th><th>w sumie</th></tr>'
         Object.values(cartItems).map(item=>{
 
             product_disp.innerHTML += `<tr class="product_row">
-            <td><ion-icon name="close-circle-outline" ></ion-icon>${item.name}</td>
+            <td><button onclick="delete_item(${i})"><ion-icon name="close-circle-outline" ></ion-icon></button>${item.name}</td>
             <td>${item.price} PLN</td>
             <td><ion-icon name="arrow-back-circle-outline"></ion-icon>
             ${item.in_cart}<ion-icon name="arrow-forward-circle-outline"></ion-icon></td>
             <td>${item.price * item.in_cart} PLN</td>
             
             </tr>
-            `
-            
+            `;
+            i++;
 
         })
        
@@ -44,10 +44,20 @@ function disp_total()
 
 }
 
-function delete_item(item)
+function delete_item(item_id)
 {
-    console.log(item);
-
+    var items = JSON.parse( localStorage.getItem("productInCart"));
+    var total = localStorage.getItem("totalCost");
+    var in_cart = parseInt( localStorage.getItem("cartNumbers"));
+    total = total - (items[item_id].price * items[item_id].in_cart);
+    in_cart -= parseInt( items[item_id].in_cart);
+    delete items[item_id];
+    localStorage.setItem("productInCart",JSON.stringify(items));
+    localStorage.setItem("totalCost",total);
+    localStorage.setItem("cartNumbers",in_cart);
+    display_cart();
+    
+    
 }
 
 
